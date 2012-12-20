153746b8d648184ba4127892a6f2dcbd5250c568
TheBizzle <jason.bertsche@gmail.com>
2012-12-19 18:11:50 -0600

    Laying groundwork for authentication

---
+++ app/controllers/Secured.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import play.api.mvc.{ Action, AnyContent, Request, RequestHeader, Result, Results, Security }
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jason
+ * Date: 12/19/12
+ * Time: 2:25 PM
+ */
+
+trait Secured {
+
+  def username(request: RequestHeader) = request.session.get(Security.username)
+
+  def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Auth.login)
+
+  def withAuth(f: => String => Request[AnyContent] => Result) =
+    Security.Authenticated(username, onUnauthorized)(user => Action(request => f(user)(request)))
+
+}
+
