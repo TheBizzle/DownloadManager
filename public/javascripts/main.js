91be73569697ce22425e74a6ad11b9457a982fd1
TheBizzle <jason.bertsche@gmail.com>
2012-12-21 14:47:19 -0600

    Further improving login page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,25 @@
 window.onload = function() {
-  $('#loginBtn').hover(
-    function(){ $(this).addClass('ui-state-hover'); },
-    function(){ $(this).removeClass('ui-state-hover'); }
-  );
+
+  var setHover = function(elem) {
+    elem.hover(
+      function(){ elem.addClass('ui-state-hover'); },
+      function(){ elem.removeClass('ui-state-hover'); }
+    );
+  };
+
+  var setMouseDown = function(elem) {
+    elem.mousedown(function(){ elem.addClass('ui-state-active'); });
+  };
+
+  var setMouseUp = function(elem) {
+    elem.mouseup(function(){ elem.removeClass('ui-state-active'); });
+  };
+
+  $('.ui-button').each(function() {
+    var elem = $(this);
+    setHover(elem);
+    setMouseDown(elem);
+    setMouseUp(elem);
+  });
+
 };
