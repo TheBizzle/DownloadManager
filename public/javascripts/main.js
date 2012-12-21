51d5e462792fb317c957aac5b48479e222df8016
TheBizzle <jason.bertsche@gmail.com>
2012-12-21 17:31:28 -0600

    Adding some more selector class inits

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,8 +6,44 @@ window.onload = function() {
   });
 
   $('.ui-tabs').each(function() {
-     var elem = $(this);
-     elem.tabs();
-  })
+    var elem = $(this);
+    elem.tabs();
+  });
+
+  $('.checkboxes').each(function() {
+    var elem = $(this);
+    elem.buttonset();
+  });
+
+  $('.check-set').each(function() {
+    var elem = $(this);
+    elem.buttonset();
+  });
+
+  $('.radio-set').each(function() {
+    var elem = $(this);
+    elem.buttonset();
+  });
+
+  $('.check-button').each(function() {
+    var elem = $(this);
+    elem.button();
+  });
+
+  $('.check-label').each(function() {
+    var elem = $(this);
+    elem.click(function() {
+      var btn = $("#" + elem.attr("for"));
+      btn[0].checked = !btn[0].checked;
+      btn.button("refresh");
+      btn.change();
+      return false;
+    });
+  });
+
+  $('.os-button').each(function() {
+    var elem = $(this);
+    elem.click();
+  });
 
 };
