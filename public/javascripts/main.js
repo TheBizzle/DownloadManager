1153c2ef25d0ec3fbcd904221e384e950852f05b
TheBizzle <jason.bertsche@gmail.com>
2012-12-21 15:08:03 -0600

    Better leveraging of jQuery UI's power

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,25 +1,13 @@
 window.onload = function() {
 
-  var setHover = function(elem) {
-    elem.hover(
-      function(){ elem.addClass('ui-state-hover'); },
-      function(){ elem.removeClass('ui-state-hover'); }
-    );
-  };
-
-  var setMouseDown = function(elem) {
-    elem.mousedown(function(){ elem.addClass('ui-state-active'); });
-  };
-
-  var setMouseUp = function(elem) {
-    elem.mouseup(function(){ elem.removeClass('ui-state-active'); });
-  };
-
   $('.ui-button').each(function() {
     var elem = $(this);
-    setHover(elem);
-    setMouseDown(elem);
-    setMouseUp(elem);
+    elem.button();
   });
+
+  $('.ui-tabs').each(function() {
+     var elem = $(this);
+     elem.tabs();
+  })
 
 };
