dc400d2a07ebd47b9ef81fb0f88fdf4568029b6e
TheBizzle <jason.bertsche@gmail.com>
2013-01-10 18:02:16 -0600

    Bringing in a library that interfaces with Google Charts

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,9 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
       "org.scalaz" %% "scalaz-core" % "7.0-SNAPSHOT",
       "mysql" % "mysql-connector-java" % "5.1.18",
-      "org.mindrot" % "jbcrypt" % "0.3m"
+      "org.mindrot" % "jbcrypt" % "0.3m",
+      "com.googlecode.charts4j" % "charts4j" % "1.4-SNAPSHOT" from
+        "http://ccl.northwestern.edu/devel/charts4j-1.4-SNAPSHOT.jar"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
