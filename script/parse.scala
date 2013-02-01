c3088ce1e3374c6984127629ba1accc56a7bcd34
TheBizzle <jason.bertsche@gmail.com>
2013-02-01 14:59:20 -0600

    Removing useless script files

--- script/parse.scala
+++
@@ -1,5 +0,0 @@
-import
-  java.io.File
-
-val file = new File("/home/jason/Desktop/NetLogoLogs/pruned/")
-file.listFiles().toSeq.par foreach (file => io.Source.fromFile(file).getLines() foreach LogParser.apply)
